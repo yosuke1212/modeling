bed3cf62ffff48660445b164f1f0a4405face072
Yousuke Nakamura <e165717@ie.u-ryukyu.ac.jp>
2018-02-15 12:07:33 +0900

    firstCommit

---
+++ jigsaw/optionController.swift
@@ -0,0 +1,35 @@
+//
+//  optionController.swift
+//  jigsaw
+//
+//  Created by 親川　弥月 on 2018/02/14.
+//  Copyright © 2018年 親川　弥月. All rights reserved.
+//
+
+import UIKit
+
+class optionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
